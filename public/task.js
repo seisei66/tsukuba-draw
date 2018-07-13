ab6bcd4e08be2353843913756cb0405dea23d671
seisei <yunku.mj.2336@gmail.com>
2018-07-13 17:47:17 +0900

    並列処理の仕様を少し変えました

--- public/task.js
+++ public/task.js
@@ -1,6 +1,5 @@
 onmessage = function (e) {
 	let pixel_data = [];
-	//console.log(e.data);
 	for (let i = 0; i < e.data.wid * e.data.hei; i++) {
 		if (e.data.af_pix.data[4 * i] != e.data.bef_pix.data[4 * i] || e.data.af_pix.data[4 * i + 1] != e.data.bef_pix.data[4 * i + 1] || e.data.af_pix.data[4 * i + 2] != e.data.bef_pix.data[4 * i + 2] || e.data.af_pix.data[4 * i + 3] != e.data.bef_pix.data[4 * i + 3]) {
 			pixel_data.push(i % e.data.wid);
