25628e96cead8e44b44fb2761eb59b018f4974fb
seisei <yunku.mj.2336@gmail.com>
2018-07-20 17:14:24 +0900

    リセットのフラグ初期化とピクセルデータの編集

--- public/pix_app.js
+++ public/pix_app.js
@@ -1,7 +1,7 @@
 onmessage = function (e) {
 	let pixels = e.data.pixels;
 	for (let i = 0; i < e.data.pixel_data.length; i += 6) {
-		let pos = (e.data.pixel_data[i] + e.data.pixel_data[i] * e.data.wid) * 4;
+		let pos = (e.data.pixel_data[i] + e.data.pixel_data[i + 1] * e.data.wid) * 4;
 		pixels.data[pos] = e.data.pixel_data[i + 2];
 		pixels.data[pos + 1] = e.data.pixel_data[i + 3];
 		pixels.data[pos + 2] = e.data.pixel_data[i + 4];
